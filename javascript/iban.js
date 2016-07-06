986339c59d8350c14ece05377cabef91f6dd0771
Viktor Johansson <shorts@live.se>
2016-07-06 16:41:37 +0200

    Add IBAN implementation

--- javascript/iban.js
+++ javascript/iban.js
@@ -4,10 +4,53 @@ export class IBAN {
   }
 
   isValid () {
-    return this.isValidFormat()
+    return this.isValidFormat() && this.isValidCountryCode() && this.isValidCheckDigits()
   }
 
   isValidFormat () {
-    return false
+    return this.iban.length > 5 && this.iban.length < 35
   }
+
+  isValidCountryCode () {
+    var strippedIban = this.iban.replace(/ /g, '')
+    return this.validCountryCodes()[strippedIban.substring(0, 2)] === strippedIban.length
+  }
+
+  isValidCheckDigits () {
+    return this.convertToDigits() % 97 === 1
+  }
+
+  convertToDigits () {
+    var iban = this.iban.replace(/ /g, '').toUpperCase()
+    var rearrangedIban = iban.substring(iban.length, 4) + iban.substring(0, 4)
+    var ibanConvertedToDigits = ''
+
+    for (var x = 0; x < rearrangedIban.length; x++) {
+      if (rearrangedIban.charCodeAt(x) < 58) {
+        ibanConvertedToDigits += rearrangedIban.charCodeAt(x) - 48
+      } else {
+        ibanConvertedToDigits += rearrangedIban.charCodeAt(x) - 55
+      }
+    }
+    return this.constructNewDigits(ibanConvertedToDigits)
+  }
+
+  constructNewDigits (ibanDigits) {
+    var iban = ibanDigits.substring(0, 9)
+
+    if (ibanDigits.length > 9) {
+      return this.constructNewDigits(parseInt(iban, 10) % 97 + '' + ibanDigits.substring(9))
+    } else {
+      return iban
+    }
+  }
+
+  validCountryCodes () {
+    var countryCodes = {
+      'SE': 24,
+      'GB': 22
+    }
+    return countryCodes
+  }
+
 }
--- javascript/test/iban_test.js
+++ javascript/test/iban_test.js
@@ -3,8 +3,8 @@ import { IBAN } from '../iban'
 import expect from 'expect'
 
 describe('iban', () => {
-  it('works', () => {
-    var iban = new IBAN('5000 0000')
+  it('is valid number', () => {
+    var iban = new IBAN('GB82 WEST 1234 5698 7654 32')
     expect(iban.isValid()).toEqual(true)
   })
 
@@ -14,12 +14,22 @@ describe('iban', () => {
   })
 
   it('is invalid for an unknown country code', () => {
-    var iban = new IBAN('WW')
+    var iban = new IBAN('SA03 8000 0000 6080 1016 7519')
     expect(iban.isValid()).toEqual(false)
   })
 
   it('is invalid for mismatching check digits', () => {
     var iban = new IBAN('SE34 5000 0000 0549 1000 0003')
     expect(iban.isValid()).toEqual(false)
+  })
+
+  it('is valid conversion', () => {
+    var iban = new IBAN('GB82 WEST 1234 5698 7654 32')
+    expect(iban.convertToDigits('GB82 WEST 1234 5698 7654 32')).toEqual('2461182')
+  })
+
+  it('is valid new digits', () => {
+    var iban = new IBAN('GB82 WEST 1234 5698 7654 32')
+    expect(iban.constructNewDigits('3214282912345698765432161182')).toEqual('2461182')
   })
 })
